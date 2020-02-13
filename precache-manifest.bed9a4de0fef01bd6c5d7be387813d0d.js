313a2d1514cc0ba803a3628d91ad46515ab1bade
VinayModepalli <vinaykumarmodepalli@gmail.com>
2020-02-14 05:19:09 +0530

    Updates

---
+++ precache-manifest.bed9a4de0fef01bd6c5d7be387813d0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb581fd10993189417704e3b0ac5c24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcd9a131e0691816dfeb",
+    "url": "/static/css/main.b022ca64.chunk.css"
+  },
+  {
+    "revision": "18cb6f6ce11f18e6fde5",
+    "url": "/static/js/2.e632dd07.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.e632dd07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcd9a131e0691816dfeb",
+    "url": "/static/js/main.817db702.chunk.js"
+  },
+  {
+    "revision": "5eb13969b7706f690f33",
+    "url": "/static/js/runtime-main.e72ee074.js"
+  }
+]);
